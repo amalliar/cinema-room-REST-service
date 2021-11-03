47ef504f8b72582f88aaa5cbae97a5a09510b794
Alexander Chetverkin <virtual-ins@mail.ru>
2021-11-03 19:21:06 +0300

    fix(controller): remove unused imports

--- src/cinema/controller/TaskController.java
+++ src/cinema/controller/TaskController.java
@@ -1,13 +1,11 @@
 package cinema.controller;
 
 import cinema.objects.Cinema;
-import cinema.objects.Seat;
 import cinema.requests.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
